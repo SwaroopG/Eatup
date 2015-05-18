16269903212c6ef1b72aa853edf6dd655065ae32
SwaroopG <swarooprajg@gmail.com>
2015-05-17 20:53:57 -0500

    Removed unlicensed picture usages. Updates to icon and background images. Minor UI updates.

--- app/src/main/java/com/poorjar/eatup/SignonActivity.java
+++ app/src/main/java/com/poorjar/eatup/SignonActivity.java
@@ -8,36 +8,83 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
 
 /**
- * TODO: Add Google and Facebook Login
+ * TODO: Add Google Login
  * Class that provides Google and Facebook login.
  */
 public class SignonActivity extends Activity {
 
+    private CallbackManager callbackManager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getApplicationContext());
+
+        callbackManager = CallbackManager.Factory.create();
+
+        registerFacebookLoginManager();
+
         setContentView(R.layout.activity_signon);
 
         Button signinButton = (Button) findViewById(R.id.sign_in_button);
         restaurantSearchIntent(signinButton);
-
-        Button facebookSigninButton = (Button) findViewById(R.id.facebook_signin_button);
-        restaurantSearchIntent(facebookSigninButton);
-
     }
 
     private void restaurantSearchIntent(final Button button) {
         button.setOnClickListener(new View.OnClickListener() {
 
             public void onClick(View arg0) {
-                Intent restaurantActivity = new Intent(getApplicationContext(), RestaurantSearchActivity.class);
-                Log.e("Hello!!!!!!!!!!!!!!", button.toString());
-                startActivity(restaurantActivity);
+                onSuccessAction();
             }
         });
+    }
+
+    /**
+     * Switch to the restaurant list menu.
+     */
+    private void onSuccessAction() {
+        Intent restaurantActivity = new Intent(getApplicationContext(), RestaurantSearchActivity.class);
+        startActivity(restaurantActivity);
+    }
+
+    /**
+     * Register the Facebook login callback.
+     */
+    private void registerFacebookLoginManager() {
+        LoginManager.getInstance().registerCallback(callbackManager,
+                new FacebookCallback<LoginResult>() {
+                    @Override
+                    public void onSuccess(LoginResult loginResult) {
+                        Log.e("OnSuccess", "OnSuccess");
+                        onSuccessAction();
+                    }
+
+                    @Override
+                    public void onCancel() {
+                        Toast.makeText(getApplicationContext(), "Login Cancel", Toast.LENGTH_LONG).show();
+                    }
+
+                    @Override
+                    public void onError(FacebookException exception) {
+                        Toast.makeText(getApplicationContext(), exception.getMessage(), Toast.LENGTH_LONG).show();
+                    }
+                });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
     }
 
     @Override
